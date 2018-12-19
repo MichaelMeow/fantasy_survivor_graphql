495666cb78c5bbc3272c6d6505871a698ed899b6
Student <example@example.com>
2018-12-19 14:30:39 -0800

    begin to add mutations back in

--- src/components/EpisodeFormContainer.jsx
+++ src/components/EpisodeFormContainer.jsx
@@ -37,6 +37,60 @@ function EpisodeFormContainer(props) {
   }
 `;
 
+function handleAddPoints(addPoints, contestant, e, number, newEpisodeID){
+  const teamReward = e.target[`teamReward${contestant.id}`].checked;
+  const teamImmunity = e.target[`teamImmunity${contestant.id}`].checked;
+  const individualReward = e.target[`individualReward${contestant.id}`].checked;
+  const individualImmunity = e.target[`individualImmunity${contestant.id}`].checked;
+  const correctVote = e.target[`correctVote${contestant.id}`].checked;
+  const recievedVote = e.target[`recievedVote${contestant.id}`].checked;
+  const out = e.target[`out${contestant.id}`].checked;
+  const recievedClue = e.target[`clue${contestant.id}`].checked;
+  const foundIdol = e.target[`foundIdol${contestant.id}`].checked;
+  const foundAdvantage = e.target[`foundAdvantage${contestant.id}`].checked;
+  const heldIdol = e.target[`heldIdol${contestant.id}`].value ? parseInt(e.target[`heldIdol${contestant.id}`].value) : 0;
+  const heldAdvantage = e.target[`heldAdvantage${contestant.id}`].checked;
+  const quoted = e.target[`quoted${contestant.id}`].checked;
+  const chosenForReward = e.target[`chosenReward${contestant.id}`].checked;
+  const juryVotes = e.target[`juryVotes${contestant.id}`].value ? parseInt(e.target[`juryVotes${contestant.id}`].value) : 0;
+  const special = e.target[`special${contestant.id}`].value ? parseInt(e.target[`special${contestant.id}`].value) : 0;
+  const total = teamReward + teamImmunity + individualReward + individualImmunity + correctVote + recievedVote + out + recievedClue + foundIdol + foundAdvantage + heldIdol + heldAdvantage + quoted + chosenForReward + juryVotes + special;
+
+  addPoints({
+    variables: {
+      contestant: contestant.id,
+      episode: newEpisodeID,
+      tribe: e.target[`tribe${contestant.id}`].value,
+      teamReward: teamReward,
+      teamImmunity: teamImmunity,
+      individualReward: individualReward,
+      individualImmunity: individualImmunity,
+      correctVote: correctVote,
+      recievedVote: recievedVote,
+      out: out,
+      recievedClue: recievedClue,
+      foundIdol: foundIdol,
+      foundAdvantage: foundAdvantage,
+      heldIdol: heldIdol,
+      heldAdvantage: heldAdvantage,
+      quoted: quoted,
+      chosenForReward: chosenForReward,
+      juryVotes: juryVotes,
+      special: special,
+      total: total,
+    }
+  })
+};
+
+function handleUpdateTribe(updateTribe, contestant, e){
+  updateTribe({
+    variables: {
+      tribe: e.target[`tribe${contestant.id}`].value,
+      contestant: contestant.id
+    }
+  })
+}
+
   async function loadFormInputs(input, client, episodes){
     input.persist();
     let episodeToLoad;
@@ -98,13 +152,46 @@ function EpisodeFormContainer(props) {
           if (error) return `Error! ${error.message}`;
           let episodes = data.episodes;
           return (
-      <ApolloConsumer>
-      {client => (
-      <form onSubmit={e => {
-        e.persist();
-        e.preventDefault();
-        return null;
+      <Mutation mutation={ADD_EPISODE} update={(cache, { data: { addEpisode } }) => {
+        const { episodes } = cache.readQuery({ query: GET_EPISODES });
+        cache.writeQuery({
+          query: GET_EPISODES,
+          data: { episodes: episodes.concat([addEpisode]) }
+        })
       }}>
+        {(addEpisode, { data }) => (
+          <Mutation mutation={ADD_POINTS}>
+          {(addPoints, { data }) => (
+            <Mutation mutation={UPDATE_TRIBE}>
+            {(updateTribe, { data }) => (
+            <ApolloConsumer>
+            {client => (
+            <form onSubmit={e => {
+              e.persist();
+              e.preventDefault();
+              if (!number.value){
+                const newEpisodeNumber = episodes.length + 1;
+                addEpisode({
+                  variables: {
+                    number: newEpisodeNumber,
+                    title: title.value,
+                    out1: out1.value,
+                    out2: out2.value,
+                    out3: out3.value,
+                    episodeMessage: episodeMessage.value,
+                    airDate: airDate.value,
+                  }
+                }).then(res => {
+                  const newEpisodeID = res.data.addEpisode.id
+                  contestants.map(contestant => {
+                    handleAddPoints(addPoints, contestant, e, number, newEpisodeID);
+                    handleUpdateTribe(updateTribe, contestant, e);
+                  })
+                })
+              };
+              client.writeData({ data: { isEpisodeSubmitted: true } })
+              return null;
+            }}>
 
       <h3>
         Enter Episode Info
@@ -183,7 +270,13 @@ function EpisodeFormContainer(props) {
         </Div>
       </form>
     )}
-    </ApolloConsumer>
+  </ApolloConsumer>
+    )}
+  </Mutation>
+  )}
+  </Mutation>
+  )}
+</Mutation>
   );
 }}
 </Query>
